e4e35b80916781adf396b9ce0e3e06a9e9b18703
Oğulcan TURAN <ismetogulcanturan@gmail.com>
2025-04-18 22:23:25 +0200

    Initialize collection as empty

--- src/OpenSettings.Docs/VersionedDocSet.cs
+++ src/OpenSettings.Docs/VersionedDocSet.cs
@@ -12,10 +12,10 @@
 
         public string PageNotFoundFilePath { get; set; }
 
-        public DocFileEntry[] Files { get; set; }
+        public DocFileEntry[] Files { get; set; } = [];
 
-        public string[] RelativePathsWithExtension { get; set; }
+        public string[] RelativePathsWithExtension { get; set; } = [];
 
-        public string[] RelativeHtmlPaths { get; set; }
+        public string[] RelativeHtmlPaths { get; set; } = [];
     }
 }
